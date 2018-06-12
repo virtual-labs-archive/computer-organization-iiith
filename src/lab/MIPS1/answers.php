775dcc457f47433acfc5bbc1b3016bad310dee41
aakarsh7599 <32263537+aakarsh7599@users.noreply.github.com>
2018-06-12 16:23:36 +0530

    Add files via upload
    Files required for quiz evaluation.

---
+++ src/lab/MIPS1/answers.php
@@ -0,0 +1,31 @@
+<?php
+echo "<html>
+<head></head>";
+echo "<body class=\"page_bg\">";
+echo '	<h1>Correct Answers</h1>
+<br>
+<b>Q1.</b>
+                <b>In MIPS we can increment the contents of a memory location in a single instruction. </b><br>
+                A.True<br>
+                <br>
+<b>Q2.</b>      <b> The maximum size of an immediate constant in add instruction is 0xFFFFFF. </b> <br>A.True <br>
+                  <br>
+<b>Q3.</b>                
+                <b> Register $r0 always contains a zero value.</b><br>
+               A.False<br>
+                <br>
+<b>Q4.</b>                
+                <b> ADDIU can generate a  overflow trap.</b><br>
+               A.True<br>
+                <br>
+				<b>Q5.</b>                
+                <b>We can use LB instruction to load a signed byte into a register and cast the signed byte to a signed word.</b><br>
+               A.False<br>
+                <br>
+</ol>';
+echo "
+</body>
+</html>";
+?>
+<body bgcolor="#E6E6FA">
+</body>
