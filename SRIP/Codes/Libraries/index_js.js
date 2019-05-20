3e2e987457bdbf2ea7d77509f7d6c0480d105c1d
Yash-Kudesia <yashku2000@gmail.com>
2019-05-20 20:29:54 +0530

    Folder reorganized finally

--- SRIP/Codes/Libraries/index_js.js
+++ SRIP/Codes/Libraries/index_js.js
@@ -74,16 +74,12 @@ var user_file_index=-101;
 
 function fileonload()
 {
-	// var while_counter=parseInt(getCookie("-1"));
-	// var user_files=parseInt(getCookie("-100"));
 	var while_counter=parseInt(sessionStorage.getItem("-1"));
 	var user_files=parseInt(sessionStorage.getItem("-100"));
 	
-		// if(getCookie("-2")=="1"){
 			if(sessionStorage.getItem("-2")=="1"){
 			copy_text_buffer="#Program to add two numbers"+"\n\n\t"+".data"+"\n\t"+"sum: .word 0"+"\n\n\t"+".text"+"\n\t"+"main:"+"\n\t"	+"li $t0, 10"+"\n\t"+"li $t1, 15"+"\n\t"+"add $t2, $t0, $t1 	# compute the sum"	+"\n\t"+"sw $t2, sum"
 		}
-		//else if(getCookie("-2")=="2"){
 			else if(sessionStorage.getItem("-2")=="2"){
 			copy_text_buffer="#Program to convert a string to int"+"\n\n"+".data"+"\n"+'string: .asciiz "13245"'+"\n"+"newline: .word 10"+"\n"+".text"+"\n"+"main:"+"\n\n"+"la $t0, string 				# Initialize S."+"\n"+"li $t2, 0 				# Initialize sum = 0."+"\n"+"lw $t5, newline"+"\n"+ "sum_loop:"+"\n\t"+"lb $t1, ($t0) 			# load the byte at addr S into $t1,"+"\n\t"+ "addu $t0, $t0, 1 		# and increment S."+"\n\t"+"beq $t1, $t5, end_sum_loop"+"\n\n\t"+"mul $t2, $t2, 10 		# t2 *= 10."+"\n\n\t"+"sub $t1, $t1, 48	 	# t1 -= '0'."+"\n\t"+"add $t2, $t2, $t1 		# t2 += t1."+"\n\n\t"+"b sum_loop # and repeat the loop."+"\n"+"end_sum_loop:"
 		}
@@ -97,14 +93,12 @@ function fileonload()
 		
 		var text=sessionStorage.getItem((i).toString());
 		var temp=parseInt(sessionStorage.getItem((user_entry).toString()));
-		//alert("temp->"+temp+" i->"+i);
 		if((user_files>=1) && (i==temp))
 		{
 			user_entry--;
 			var nam=sessionStorage.getItem((user_entry).toString());
 			user_entry--;
 			myfunctionlist(text,nam);
-			alert("name-:"+nam);
 			user_files--;
 		}
 		else{
@@ -131,7 +125,6 @@ function init(text_node)
 		name1.text=text_node;
 	}
 	names.push(name1);
-	//setCookie("-1",(count-1).toString(), 0.015625);
 	sessionStorage.setItem("-1",(count-1).toString());
 	return names;
 }
@@ -165,14 +158,12 @@ function clicklistner(y)
 	var node=document.getElementById("mytext");
 	last_button_id=x;
 	node.value=names[x].text;
-	//setCookie((x).toString(), names[x].text, 0.015625);
 	sessionStorage.setItem((x).toString(),names[x].text);
 }
 function textareachanged()
 {
 	var node=document.getElementById("mytext");
 	names[last_button_id].text=node.value;
-//	setCookie((last_button_id).toString(), names[last_button_id].text, 0.015625);
 	sessionStorage.setItem((last_button_id).toString(),names[last_button_id].text);
 }
 // //******************************************************************************************************
@@ -225,17 +216,14 @@ function copy_code()
 	copy_text_buffer=node.value;
 	if(copy_text_buffer=="#Program to add two numbers"+"\n\n\t"+".data"+"\n\t"+"sum: .word 0"+"\n\n\t"+".text"+"\n\t"+"main:"+"\n\t"	+"li $t0, 10"+"\n\t"+"li $t1, 15"+"\n\t"+"add $t2, $t0, $t1 	# compute the sum"	+"\n\t"+"sw $t2, sum"
 	){
-		//setCookie("-2","1", 0.015625);
 		sessionStorage.setItem("-2","1");
 	}
 	else if(copy_text_buffer=="#Program to convert a string to int"+"\n\n"+".data"+"\n"+'string: .asciiz "13245"'+"\n"+"newline: .word 10"+"\n"+".text"+"\n"+"main:"+"\n\n"+"la $t0, string 				# Initialize S."+"\n"+"li $t2, 0 				# Initialize sum = 0."+"\n"+"lw $t5, newline"+"\n"+ "sum_loop:"+"\n\t"+"lb $t1, ($t0) 			# load the byte at addr S into $t1,"+"\n\t"+ "addu $t0, $t0, 1 		# and increment S."+"\n\t"+"beq $t1, $t5, end_sum_loop"+"\n\n\t"+"mul $t2, $t2, 10 		# t2 *= 10."+"\n\n\t"+"sub $t1, $t1, 48	 	# t1 -= '0'."+"\n\t"+"add $t2, $t2, $t1 		# t2 += t1."+"\n\n\t"+"b sum_loop # and repeat the loop."+"\n"+"end_sum_loop:"
 	){
-		//setCookie("-2","2", 0.015625);
 		sessionStorage.setItem("-2","2");
 	}
 	else if(copy_text_buffer="#compute length of a string"+"\n\n"+".data"+"\n"+'string: .asciiz "This is a string"'+"\n"+"length: .word 0"+"\n\n"+".text"+"\n"+"la $t1, string"+"\n"+"li $t2, 0"+"\n"+"length_loop:"+"\n\t"+"lb $t3, ($t1)"+"\n\t"+"beqz $t3, endloop"+"\n\t"+"addu $t2, $t2, 1"+"\n\t"+"addu $t1, $t1, 1"+"\n\t"+"b length_loop"+"\n"+"endloop:"+"\n\t"+"sub $t2, $t2, 1		#subtract 1 to ignore \\0 "+"\n\t"+"sw $t2, length"
 	){
-		//setCookie("-2","3", 0.015625);
 		sessionStorage.setItem("-2","3");
 	}
 	document.execCommand("copy");
@@ -354,12 +342,9 @@ function setinputfileintextarea()
 			contents=event.target.result;
 			myfunctionlist(contents,file.name);
 			user_file_counter++;
-			//setCookie("-100",(user_file_counter).toString(), 0.015625);
-			//setCookie((user_file_index).toString(),(count-2).toString(), 0.015625);//storing the cout at which user has opened a local file
 			sessionStorage.setItem("-100",(user_file_counter).toString());
 			sessionStorage.setItem((user_file_index).toString(),(count-2).toString());
 			user_file_index--;
-			//setCookie((user_file_index).toString(),file.name, 0.015625); //storing the name of user file to load again properly
 			sessionStorage.setItem((user_file_index).toString(),file.name);
 			user_file_index--;
 		};
@@ -375,23 +360,6 @@ function exit_Window() {
 	alert("Functionality not implemented\nWe can only close that window which was opened by javascript");
   }
 //********************************************************************************************* */
-//Undo and Redo fucntionality
-// var stack=[]
-// var stacktop=-1;	//current index of stack
-// var undo_redo_counter=0;
-// //adding the inital entry
-// var node_init=document.createElement("TEXTAREA");
-// var text_init = document.createTextNode("");
-// node_init.appendChild(text_init);
-// stack.push(node);
-// stacktop++;
-
-// function addInStack()
-// {
-// 	var el=document.getElementById("mytext");
-// 	stacktop+=1;
-// 	stack.push(el);
-// }
 function undo_fun()
 {
 	alert("Functionality not implemented");
@@ -401,28 +369,3 @@ function redo_fun()
 	alert("Functionality not implemented");
 }
 //******************************************************************************************************** */
-//Using cokkie to retrive the data of file
-
-// function setCookie(cname,cvalue,exdays) {
-// 	var d = new Date();
-// 	d.setTime(d.getTime() + (exdays*24*60*60*1000));
-// 	var expires = "expires=" + d.toGMTString();
-// 	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-// }
-  
-// function getCookie(cname) {
-// 	var name = cname + "=";
-// 	var decodedCookie = decodeURIComponent(document.cookie);
-// 	var ca = decodedCookie.split(';');
-// 	for(var i = 0; i < ca.length; i++) {
-// 	  var c = ca[i];
-// 	  // alert("String stored is-:"+c);
-// 	  while (c.charAt(0) == ' ') {
-// 		c = c.substring(1);
-// 	  }
-// 	  if (c.indexOf(name) == 0) {
-// 		return c.substring(name.length, c.length);
-// 	  }
-// 	}
-// 	return "";
-// }
