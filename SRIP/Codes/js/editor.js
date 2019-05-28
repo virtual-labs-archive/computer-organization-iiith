9145610ecc01d2c1be79a05c51f360f6ecb9a3de
nsrilekha <srilekha.n16@iiits.in>
2019-05-28 21:37:24 +0530

    added all UI files regarding my experiment

--- SRIP/Codes/js/editor.js
+++ SRIP/Codes/js/editor.js
@@ -13,5 +13,5 @@ $(".nav-tabs").on("click", "a", function(e){
         e.preventDefault();
         var id = $(".nav-tabs").children().length; //think about it ;)
         $(this).closest('li').before('<li><a href="#contact">arm'+id+'</a><span>x</span></li>');         
-        $('.tab-content').append('<div class="tab-pane" id="contact"><form action="/action_page.php"><textarea name="textarea" rows="27" cols="10">Write something here</textarea><br></form></div>');
+        $('.tab-content').append('<div class="tab-pane" id="contact"><form action="/action_page.php"><textarea name="comments" id="comments" style="width:92%;padding:2%;height:400px; border:10px double yellowgreen;"></textarea><br></form></div>');
 });
